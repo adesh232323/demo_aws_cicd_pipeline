d069122f5a1434efee96ed7ce85d98ba5455e692
Vikas Patidar <adesh@mindpathtech.com>
2024-09-19 18:57:40 +0530

    initial commit

---
+++ lib/demo-cicd-pipeline-cdk-stack.ts
@@ -0,0 +1,25 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  CodePipeline,
+  CodePipelineSource,
+  ShellStep,
+} from "aws-cdk-lib/pipelines";
+
+export class DemoCicdPipelineCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const demoCICDPipline = new CodePipeline(this, "demopipline", {
+      synth: new ShellStep("Synth", {
+        // Use a connection created using the AWS console to authenticate to GitHub
+        // Other sources are available.
+        input: CodePipelineSource.gitHub(
+          "adesh232323/demo_aws_cicd_pipeline",
+          "master"
+        ),
+        commands: ["npm ci", "npm run build", "npx cdk synth"],
+      }),
+    });
+  }
+}
